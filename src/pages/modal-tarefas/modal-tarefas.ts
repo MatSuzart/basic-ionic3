2809dee4073ad59f35294857f7cfb098b2dedf14
MathSuzart <mathsuzart@outlook.com>
2017-05-05 15:12:28 -0300

    Update modal-tarefas.ts

--- src/pages/modal-tarefas/modal-tarefas.ts
+++ src/pages/modal-tarefas/modal-tarefas.ts
@@ -1,30 +1,26 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+ import { Component } from '@angular/core';
+ import { IonicPage, NavController, NavParams } from 'ionic-angular';
+ import { TarefasPage } from '../tarefas/tarefas';
 
-@IonicPage()
-@Component({
-  selector: 'page-modal-tarefas',
-  templateUrl: 'modal-tarefas.html',
-})
-export class ModalTarefas {
-  
-  tarefa: any;
-  
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+ @IonicPage()
+ @Component({
+   selector: 'page-modal-tarefas',
+    templateUrl: 'modal-tarefas.html',
+  })
+  export class ModalTarefas {
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ModalTarefas');
-  }
-  
-  callPush(page, params = {}){
-    this.navCtrl.setRoot(this);
-    this.navCtrl.push(page, params)
-  }
 
-  salvar() {
-    /*this.view.dismiss(this.tarefa);*/
-    this.callPush(TarefasPage, {tarefa: this.tarefa});
-  }
+   tarefa: any;
 
-}
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+    }
+
+    ionViewDidLoad() {
+      console.log('ionViewDidLoad ModalTarefas');
+    }
+
+    salvar() {
+     this.navCtrl.push(TarefasPage, {discricao: this.tarefa})
+    }
+
+  }
